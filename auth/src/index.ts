61447a8063a0dbc5806f6bbb7bd4e1016a9a8511
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-10-15 17:26:12 +0200

    K8S Setup Kubernetes-Ingress Service to Handle Load Balancing and Expose Service to World

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,4 +6,8 @@ const PORT = 3000;
 
 app.use(json());
 
+app.get('/api/users/currentUser', (req, res) => {
+  return res.send('HI There it Works Fine');
+});
+
 app.listen(PORT, () => console.log(`Auth Service Listening on Port ${PORT} Test Skaffold`));
