a33e878c74ee1dc3c2794a124d98963826807b82
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-10-15 17:31:09 +0200

    Adding Auth Service Router Skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import { json } from 'body-parser';
 
-const app = express();
-const PORT = 3000;
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
+const app = express();
 app.use(json());
 
-app.get('/api/users/currentUser', (req, res) => {
-  return res.send('HI There it Works Fine');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.listen(PORT, () => console.log(`Auth Service Listening on Port ${PORT} Test Skaffold`));
+app.listen(3000, () => {
+  console.log('Auth Service Listening on Port 3000');
+});
