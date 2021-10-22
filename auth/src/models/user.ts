3666034fbb5e3c518a93c4fce437316dfb3c72e3
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-10-22 14:22:34 +0200

    JWT Vs Cookie Explain The Different Between Them

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 /**
  * An Interface that Describes the Properties
@@ -35,6 +36,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashedPassword = await Password.toHash(this.get('password'));
+    this.set('password', hashedPassword);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
