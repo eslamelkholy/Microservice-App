4797e6138975a1150df96e1a3c6e9e9524ffba2f
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-10-23 14:24:20 +0200

    Setup Nextjs Client Services with Test Routers

---
+++ client/pages/banana.js
@@ -0,0 +1,5 @@
+const banana = () => {
+  return <h1>Hello Banana</h1>;
+};
+
+export default banana;
---
+++ client/pages/index.js
@@ -0,0 +1,5 @@
+const landing = () => {
+  return <h1>Hello Landing Page</h1>;
+};
+
+export default landing;
